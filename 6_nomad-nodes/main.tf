c1df0262f2e95c9937031a30d373045339dd28f9
Ben Barkhouse <bfbarkhouse@live.com>
2024-12-20 10:45:52 -0500

    added ingress rule from the alb to itself on all ports

--- 6_nomad-nodes/main.tf
+++ 6_nomad-nodes/main.tf
@@ -315,6 +315,13 @@ resource "aws_security_group" "nomad_traefik_lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
   egress {
     from_port   = 8080
     to_port     = 8080

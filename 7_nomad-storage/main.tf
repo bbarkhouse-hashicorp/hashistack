4ca91935bd304b74cf1990b42b533c137ec15409
Ben Barkhouse <bfbarkhouse@live.com>
2025-01-09 10:10:04 -0500

    nomad csi

---
+++ 7_nomad-storage/main.tf
@@ -0,0 +1,133 @@
+terraform {
+  required_providers {
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.6"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.8.0"
+    }
+
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.66.0"
+    }
+
+    vault = {
+      source = "hashicorp/vault"
+      version = "~> 3.18.0"
+    }
+
+    nomad = {
+      source = "hashicorp/nomad"
+      version = "2.0.0-beta.1"
+    }
+  }
+}
+
+provider "doormat" {}
+
+provider "hcp" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::${var.aws_account_id}:role/tfc-doormat-role_6_nomad-nodes"
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+}
+
+provider "vault" {}
+
+data "vault_kv_secret_v2" "bootstrap" {
+  mount = data.terraform_remote_state.nomad_cluster.outputs.bootstrap_kv
+  name  = "nomad_bootstrap/SecretID"
+}
+
+provider "nomad" {
+  address = data.terraform_remote_state.nomad_cluster.outputs.nomad_public_endpoint
+  secret_id = data.vault_kv_secret_v2.bootstrap.data["SecretID"]
+}
+
+data "terraform_remote_state" "networking" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_organization
+    workspaces = {
+      name = "1_networking"
+    }
+  }
+}
+
+data "terraform_remote_state" "hcp_clusters" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_organization
+    workspaces = {
+      name = "2_hcp-clusters"
+    }
+  }
+}
+
+data "terraform_remote_state" "nomad_cluster" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_organization
+    workspaces = {
+      name = "5_nomad-cluster"
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "mount_ebs_volumes" {
+  name   = "mount-ebs-volumes"
+  role   = aws_iam_role.instance_role.id
+  policy = data.aws_iam_policy_document.mount_ebs_volumes.json
+}
+
+data "aws_iam_policy_document" "mount_ebs_volumes" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:DescribeInstances",
+      "ec2:DescribeTags",
+      "ec2:DescribeVolumes",
+      "ec2:AttachVolume",
+      "ec2:DetachVolume",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_ebs_volume" "nomad" {
+  availability_zone = aws_instance.client[0].availability_zone
+  size              = 40
+}
+
+output "ebs_volume" {
+    value = <<EOM
+# volume registration
+type        = "csi"
+id          = "nomad"
+name        = "nomad"
+external_id = "${aws_ebs_volume.nomad.id}"
+plugin_id   = "aws-ebs0"
+
+capability {
+  access_mode     = "single-node-writer"
+  attachment_mode = "file-system"
+}
+EOM
+}
+
+

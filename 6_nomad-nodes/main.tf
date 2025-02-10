b0b9be00c7d954fd19c27bc2602adace9c71fb2f
Ben Barkhouse <bfbarkhouse@live.com>
2025-02-10 09:16:48 -0500

    updated dlm iam role

--- 6_nomad-nodes/main.tf
+++ 6_nomad-nodes/main.tf
@@ -374,51 +374,11 @@ resource "aws_autoscaling_attachment" "arm_node_asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.nomad_client_arm_asg.id
   lb_target_group_arn    = aws_alb_target_group.nomad_traefik_alb_tg.arn
 }
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["dlm.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+data "aws_iam_role" "dlm_lifecycle_role" {
+  name = "dlm-lifecycle-role" 
 }
 
-# resource "aws_iam_role" "dlm_lifecycle_role" {
-#   name               = "dlm-lifecycle-role"
-#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-# }
-
-# data "aws_iam_policy_document" "dlm_lifecycle" {
-#   statement {
-#     effect = "Allow"
-
-#     actions = [
-#       "ec2:CreateSnapshot",
-#       "ec2:CreateSnapshots",
-#       "ec2:DeleteSnapshot",
-#       "ec2:DescribeInstances",
-#       "ec2:DescribeVolumes",
-#       "ec2:DescribeSnapshots",
-#     ]
-
-#     resources = ["*"]
-#   }
-
-#   statement {
-#     effect    = "Allow"
-#     actions   = ["ec2:CreateTags"]
-#     resources = ["arn:aws:ec2:*::snapshot/*"]
-#   }
-# }
-# resource "aws_iam_role_policy" "dlm_lifecycle" {
-#   name   = "dlm-lifecycle-policy"
-#   role   = aws_iam_role.dlm_lifecycle_role.id
-#   policy = data.aws_iam_policy_document.dlm_lifecycle.json
-# }
 resource "aws_dlm_lifecycle_policy" "x86_snapshots" {
   description = "Nomad x86 client snapshot policy"
   execution_role_arn = aws_iam_role.dlm_lifecycle_role.arn
